5a5e5b7dd8854539645b3f52834ace0fb2e5bed3
jbxcruz <johnbertcruz2020@gmail.com>
2024-12-16 15:37:20 +0800

    Update doodlejump.js

--- doodlejump.js
+++ doodlejump.js
@@ -1,7 +1,13 @@
+
+
+
+
+
 let board;
 let boardWidth = 360;
 let boardHeight = 576;
 let context;
+
 
 let doodlerWidth = 46;
 let doodlerHeight = 46;
@@ -18,16 +24,18 @@ let doodler = {
     height: doodlerHeight
 };
 
+
 let velocityX = 0;
 let velocityY = 0; 
 let initialVelocityY = -8; 
 let gravity = 0.4;
 
+
 let platformArray = [];
 let platformWidth = 60;
 let platformHeight = 18;
 let platformImg;
-let platformBrokenImg; // For broken platform image
+
 
 let stars = [];
 let numStars = 100;
@@ -36,20 +44,16 @@ let score = 0;
 let maxScore = 0;
 let gameOver = false;
 
-let shakeOffsetX = 0;
-let shakeOffsetY = 0;
-let shakeDuration = 0;
-
-let platformsPassed = 0; // For tracking platforms passed
-
 window.onload = function () {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
-    context = board.getContext("2d");
+    context = board.getContext("2d"); 
 
+ 
     board.style.margin = "auto";
     board.style.display = "block";
+
 
     doodlerRightImg = new Image();
     doodlerRightImg.src = "./doodler-right.png";
@@ -63,9 +67,6 @@ window.onload = function () {
 
     platformImg = new Image();
     platformImg.src = "./platform.png";
-    
-    platformBrokenImg = new Image(); // Load broken platform image
-    platformBrokenImg.src = "./platform-broken.png";
 
     velocityY = initialVelocityY;
     placePlatforms();
@@ -77,14 +78,14 @@ window.onload = function () {
 function update() {
     requestAnimationFrame(update);
     if (gameOver) {
-        shakeScreen(); // Trigger screen shake when the game is over
         return;
     }
     context.clearRect(0, 0, board.width, board.height);
 
+   
     drawStars();
 
-    // Doodler Movement
+    
     doodler.x += velocityX;
     if (doodler.x > boardWidth) {
         doodler.x = 0;
@@ -99,35 +100,25 @@ function update() {
     }
     context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
 
-    // Handle Platforms
+    
     for (let i = 0; i < platformArray.length; i++) {
         let platform = platformArray[i];
-
         if (velocityY < 0 && doodler.y < boardHeight * 3 / 4) {
-            platform.y -= initialVelocityY; // Slide platform down
+            platform.y -= initialVelocityY; //slide platform down
         }
-
         if (detectCollision(doodler, platform) && velocityY >= 0) {
-            velocityY = initialVelocityY; // Jump when landing on platform
-
-            if (!platform.broken) {
-                platform.img = platformBrokenImg; // Set platform to broken image
-                platform.broken = true; // Mark platform as broken
-                platformsPassed++; // Count platform passed
-                score = platformsPassed * 10; // Increment score by 10 points per platform passed
-            }
+            velocityY = initialVelocityY; //jump
         }
-
         context.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height);
     }
 
-    // Remove platforms that are off-screen and create new ones
+    
     while (platformArray.length > 0 && platformArray[0].y >= boardHeight) {
-        platformArray.shift(); // Removes first element from the array
-        newPlatform(); // Replace with new platform on top
+        platformArray.shift(); //removes first element from the array
+        newPlatform(); //replace with new platform on top
     }
 
-    // Score display
+    //score
     updateScore();
     context.fillStyle = "black";
     context.font = "16px sans-serif";
@@ -139,14 +130,14 @@ function update() {
 }
 
 function moveDoodler(e) {
-    if (e.code == "ArrowRight" || e.code == "KeyD") { // Move right
+    if (e.code == "ArrowRight" || e.code == "KeyD") { //move right
         velocityX = 4;
         doodler.img = doodlerRightImg;
-    } else if (e.code == "ArrowLeft" || e.code == "KeyA") { // Move left
+    } else if (e.code == "ArrowLeft" || e.code == "KeyA") { //move left
         velocityX = -4;
         doodler.img = doodlerLeftImg;
     } else if (e.code == "Space" && gameOver) {
-        // Reset game on Space key
+        //reset
         doodler = {
             img: doodlerRightImg,
             x: doodlerX,
@@ -158,7 +149,7 @@ function moveDoodler(e) {
         velocityX = 0;
         velocityY = initialVelocityY;
         score = 0;
-        platformsPassed = 0;
+        maxScore = 0;
         gameOver = false;
         placePlatforms();
     }
@@ -167,18 +158,17 @@ function moveDoodler(e) {
 function placePlatforms() {
     platformArray = [];
 
-    // Starting platform
+   
     let platform = {
         img: platformImg,
         x: boardWidth / 2 - platformWidth / 2,
         y: boardHeight - platformHeight - 10,
         width: platformWidth,
-        height: platformHeight,
-        broken: false
+        height: platformHeight
     };
     platformArray.push(platform);
 
-    // Random platforms
+    
     for (let i = 1; i <= 6; i++) {
         let randomX = Math.random() * (boardWidth - platformWidth); 
         let randomY = boardHeight - i * 100; 
@@ -188,8 +178,7 @@ function placePlatforms() {
             x: randomX,
             y: randomY,
             width: platformWidth,
-            height: platformHeight,
-            broken: false
+            height: platformHeight
         };
 
         platformArray.push(platform);
@@ -203,8 +192,7 @@ function newPlatform() {
         x: randomX,
         y: -platformHeight, 
         width: platformWidth,
-        height: platformHeight,
-        broken: false
+        height: platformHeight
     };
 
     platformArray.push(platform);
@@ -229,6 +217,7 @@ function updateScore() {
     }
 }
 
+
 function generateStars() {
     for (let i = 0; i < numStars; i++) {
         stars.push({
@@ -246,23 +235,11 @@ function drawStars() {
         context.arc(star.x, star.y, star.radius, 0, 2 * Math.PI);
         context.fill();
 
+       
         star.y += 0.5;
         if (star.y > boardHeight) {
             star.y = 0;
             star.x = Math.random() * boardWidth;
         }
     }
-}
-
-function shakeScreen() {
-    if (shakeDuration > 0) {
-        shakeOffsetX = (Math.random() - 0.5) * 10; // Random shake offset
-        shakeOffsetY = (Math.random() - 0.5) * 10;
-        shakeDuration--;
-    } else {
-        shakeOffsetX = 0;
-        shakeOffsetY = 0;
-    }
-
-    context.translate(shakeOffsetX, shakeOffsetY);
 }
