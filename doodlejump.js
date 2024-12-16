b82e15d618bda9bffcdd62fa7049064b564d7a3e
jbxcruz <johnbertcruz2020@gmail.com>
2024-12-16 16:00:37 +0800

    Update doodlejump.js

--- doodlejump.js
+++ doodlejump.js
@@ -22,9 +22,9 @@ let doodler = {
 
 let velocityX = 0;
 let velocityY = 0;
-let jumpVelocity = -8;  // Initial upward velocity for jumping
-let bounceGravity = 0.4; // Reduced gravity when going up (to make the jump faster)
-let fallGravity = 0.10;   // Increased gravity when falling (to make the fall slower)
+let jumpVelocity = -10;  // Initial upward velocity for jumping
+let bounceGravity = 0.5; // Reduced gravity when going up (to make the jump faster)
+let fallGravity = 0.8;   // Increased gravity when falling (to make the fall slower)
 
 let platformArray = [];
 let platformWidth = 60;
