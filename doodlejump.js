af0c417b37c482f49a966e25165dc71f9a28fd1d
jbxcruz <johnbertcruz2020@gmail.com>
2024-12-17 13:38:26 +0800

    Update doodlejump.js

--- doodlejump.js
+++ doodlejump.js
@@ -104,12 +104,17 @@ function update() {
             platform.y -= jumpVelocity;
         }
 
-        if (detectCollision(doodler, platform) && velocityY >= 0) {
-            if (platform.isBreakable) {
-                toRemove.push(i);
-            }
+        
+    if (detectCollision(doodler, platform) && velocityY >= 0) {
+        if (platform.isBreakable) {
+            // Break the platform and let the character fall
+            platformArray.splice(i, 1); // Remove the platform immediately
+        } else {
+            // Regular platform: bounce the character up
             velocityY = jumpVelocity;
         }
+    }
+
 
         context.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height);
     }
