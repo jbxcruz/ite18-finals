1a8861be86d549d29a02be09cebe5cfffddabf86
jbxcruz <johnbertcruz2020@gmail.com>
2024-12-18 21:55:45 +0800

    Update doodlejump.js

--- doodlejump.js
+++ doodlejump.js
@@ -218,7 +218,7 @@ function placePlatforms() {
     platformArray.push(platform);
 
     
-    const minVerticalDistance = 30;
+    const minVerticalDistance = 20;
     const minHorizontalSpacing = 50;
     let currentX = platform.x;
 
@@ -250,8 +250,8 @@ function placePlatforms() {
 
 
 function newPlatform() {
-    const minJumpableDistance = 30;
-    const maxJumpableDistance = 40;
+    const minJumpableDistance = 20;
+    const maxJumpableDistance = 30;
 
     let randomX = Math.random() * (boardWidth - platformWidth);
     let randomY = -Math.random() * (maxJumpableDistance - minJumpableDistance) - minJumpableDistance;
